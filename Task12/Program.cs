c11a925c8a63ff7fdbfdae64f8ff80bc0fb65986
Karina <kr.vorontsova@yandex.ru>
2023-01-19 15:14:39 +0500

    tasks 12-16

---
+++ Task12/Program.cs
@@ -0,0 +1,23 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 12 мин
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите число a ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Multiplicity(number1, number2);
+
+void Multiplicity(int arg1, int arg2)
+{
+    if (arg1 % arg2 == 0)
+    Console.WriteLine(" Число " + arg1 + " кратно " + arg2);
+    else 
+    Console.WriteLine(" Число " + arg1 + " не кратно " + arg2 + " остаток = " + arg1 % arg2);
+}
---
+++ Task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+bool mult = Mult(number1);
+Console.WriteLine(mult?"ДА":"НЕТ");
+
+bool Mult(int num1) 
+{
+    return num1 % 7 == 0 && number1 % 23 == 0;
+}
+
