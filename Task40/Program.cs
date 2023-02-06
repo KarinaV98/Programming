93491ac9621f6e7309ad6724b287f551239b4de8
Karina <kr.vorontsova@yandex.ru>
2023-02-06 14:45:31 +0500

    added tesks 40 & 42

---
+++ Task40/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+
+
+
+bool TriangleCheck(int a, int b, int c)
+{
+    if (a < (b+c) && b < (c+a) && c < (a+b)) return true;
+    else return false;
+}
+
+Console.WriteLine ("Введите три стороны тркугольника.");
+Console.Write("Введите сторону a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите сторону c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+bool res = TriangleCheck(a, b, c);
+Console.WriteLine(res ? "Да": "Нет");
+
---
+++ Task42/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int NumBinary(int num)
+{
+    int binar = 0;
+    int count = 1;
+    while (num > 0)
+    {
+        binar = binar + (num % 2 * count);
+        num /=2;
+        count*=10;
+    }
+    return binar;
+    
+        
+
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = NumBinary(number);
+Console.WriteLine(result);
